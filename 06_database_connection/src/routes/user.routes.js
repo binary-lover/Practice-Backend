b6536782dcf7e6b86e108a7f85a89cf5ef3e4f28
binary-lover <whitedevil.hamd@gmail.com>
2024-07-28 16:37:41 +0530

    add new rotuer for user controller

--- 06_database_connection/src/routes/user.routes.js
+++ 06_database_connection/src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessTocken, registerUser } from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logOutUser,
+    refreshAccessTocken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -7,21 +19,30 @@ const router = Router();
 
 router.route("/register").post(
     upload.fields([
-        { name: "avatar",
-        maxCount: 1
-        },
+        { name: "avatar", maxCount: 1 },
         {
             name: "coverImage",
-            maxCount: 1
-        }
+            maxCount: 1,
+        },
     ]),
     registerUser
-)
+);
 
 router.route("/login").post(loginUser);
 
 // secured routes
 
-router.route("/logout").post(verifyJWT,logOutUser)
-router.route("/refresh-token").post(refreshAccessTocken)
+router.route("/logout").post(verifyJWT, logOutUser);
+router.route("/refresh-token").post(refreshAccessTocken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-aacount").patch(verifyJWT, updateAccountDetails);
+router
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 export default router;
